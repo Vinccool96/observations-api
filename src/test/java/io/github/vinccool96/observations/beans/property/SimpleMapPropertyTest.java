5093cc314baf61f94d63b170660882314e40dec5
Vincent Girard <vincent.girard.7@ulaval.ca>
2021-01-12 13:35:02 -0500

    Forgotten tests

---
+++ src/test/java/io/github/vinccool96/observations/beans/property/SimpleMapPropertyTest.java
@@ -0,0 +1,82 @@
+package io.github.vinccool96.observations.beans.property;
+
+import io.github.vinccool96.observations.collections.ObservableCollections;
+import io.github.vinccool96.observations.collections.ObservableMap;
+import io.github.vinccool96.observations.util.Pair;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class SimpleMapPropertyTest {
+
+    private static final Object DEFAULT_BEAN = null;
+
+    private static final String DEFAULT_NAME = "";
+
+    private static final ObservableMap<Object, Object> DEFAULT_VALUE = null;
+
+    private static final ObservableMap<Object, Object> VALUE_1 =
+            ObservableCollections.observableHashMap(new Pair<Object, Object>(new Object(), new Object()));
+
+    @Test
+    public void testConstructor_NoArguments() {
+        final MapProperty<Object, Object> v = new SimpleMapProperty<Object, Object>();
+        assertEquals(DEFAULT_BEAN, v.getBean());
+        assertEquals(DEFAULT_NAME, v.getName());
+        assertEquals(DEFAULT_VALUE, v.get());
+    }
+
+    @Test
+    public void testConstructor_InitialValue() {
+        final MapProperty<Object, Object> v1 = new SimpleMapProperty<Object, Object>(VALUE_1);
+        assertEquals(DEFAULT_BEAN, v1.getBean());
+        assertEquals(DEFAULT_NAME, v1.getName());
+        assertEquals(VALUE_1, v1.get());
+
+        final MapProperty<Object, Object> v2 = new SimpleMapProperty<Object, Object>(DEFAULT_VALUE);
+        assertEquals(DEFAULT_BEAN, v2.getBean());
+        assertEquals(DEFAULT_NAME, v2.getName());
+        assertEquals(DEFAULT_VALUE, v2.get());
+    }
+
+    @Test
+    public void testConstructor_Bean_Name() {
+        final Object bean = new Object();
+        final String name = "My name";
+        final MapProperty<Object, Object> v = new SimpleMapProperty<Object, Object>(bean, name);
+        assertEquals(bean, v.getBean());
+        assertEquals(name, v.getName());
+        assertEquals(DEFAULT_VALUE, v.get());
+
+        final MapProperty<Object, Object> v2 = new SimpleMapProperty<Object, Object>(bean, null);
+        assertEquals(bean, v2.getBean());
+        assertEquals(DEFAULT_NAME, v2.getName());
+        assertEquals(DEFAULT_VALUE, v2.get());
+    }
+
+    @Test
+    public void testConstructor_Bean_Name_InitialValue() {
+        final Object bean = new Object();
+        final String name = "My name";
+        final MapProperty<Object, Object> v1 = new SimpleMapProperty<Object, Object>(bean, name, VALUE_1);
+        assertEquals(bean, v1.getBean());
+        assertEquals(name, v1.getName());
+        assertEquals(VALUE_1, v1.get());
+
+        final MapProperty<Object, Object> v2 = new SimpleMapProperty<Object, Object>(bean, name, DEFAULT_VALUE);
+        assertEquals(bean, v2.getBean());
+        assertEquals(name, v2.getName());
+        assertEquals(DEFAULT_VALUE, v2.get());
+
+        final MapProperty<Object, Object> v3 = new SimpleMapProperty<Object, Object>(bean, null, VALUE_1);
+        assertEquals(bean, v3.getBean());
+        assertEquals(DEFAULT_NAME, v3.getName());
+        assertEquals(VALUE_1, v3.get());
+
+        final MapProperty<Object, Object> v4 = new SimpleMapProperty<Object, Object>(bean, null, DEFAULT_VALUE);
+        assertEquals(bean, v4.getBean());
+        assertEquals(DEFAULT_NAME, v4.getName());
+        assertEquals(DEFAULT_VALUE, v4.get());
+    }
+
+}
